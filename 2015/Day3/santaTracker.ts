3a2ae0dbbc20d151bc0df769013f3bfc662576f0
Matt Haws <matthaws@gmail.com>
2023-12-03 19:51:24 -0500

    2015 Day 3

--- 2015/Day3/santaTracker.ts
+++ 2015/Day3/santaTracker.ts
@@ -1,3 +1,40 @@
 const inputParser = require("../../inputParser.ts");
-const packageInput = inputParser("day-2.txt", "text");
+const packageInput = inputParser("day-3.txt", "text");
 
+const trackNumDeliveries = (path: string) => {
+  let santaX = 0;
+  let santaY = 0;
+
+  let roboSantaX = 0;
+  let roboySantaY = 0;
+
+  let turn = "SANTA";
+
+  const houseTracker = new Set<string>();
+  houseTracker.add("0:0");
+
+  path.split("").forEach((char) => {
+    let difX = 0;
+    let difY = 0;
+    if (char === "^") difX = 1;
+    if (char === "v") difX = -1;
+    if (char === "<") difY = -1;
+    if (char === ">") difY = 1;
+
+    if (turn === "SANTA") {
+      santaX = santaX + difX;
+      santaY = santaY + difY;
+      houseTracker.add(`${santaX}:${santaY}`);
+      turn = "ROBOT";
+    } else {
+      roboSantaX = roboSantaX + difX;
+      roboySantaY = roboySantaY + difY;
+      houseTracker.add(`${roboSantaX}:${roboySantaY}`);
+      turn = "SANTA";
+    }
+  });
+
+  return houseTracker.size;
+};
+
+console.log(trackNumDeliveries(packageInput[0]));
